dd28e169b8d896d195e51643c0df227c86a07f4c
JiWon <jjw8969@naver.com>
2022-12-22 17:40:38 +0900

    실습 * 회원 Repository 작성(ID, Name) * 회원 Repository 조회 * Test Case 작성

---
+++ src/main/java/Training/Training/spring/repository/MemberRepository.java
@@ -0,0 +1,14 @@
+package Training.Training.spring.repository;
+
+import Training.Training.spring.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    Member save(Member member);
+    Optional<Member> findById(Long id);
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
+
+}
---
+++ src/main/java/Training/Training/spring/repository/MemoryMemberRepository.java
@@ -0,0 +1,41 @@
+package Training.Training.spring.repository;
+
+import Training.Training.spring.domain.Member;
+
+import java.util.*;
+
+public class MemoryMemberRepository implements MemberRepository {
+
+    private static Map<Long, Member> store = new HashMap<>();
+    private static long sequence = 0L;
+    @Override
+    public Member save(Member member) {
+        member.setId(++sequence);
+        store.put(member.getId(),member);
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        return Optional.ofNullable(store.get(id)); // Null이 반환될 수 있기 때문에 Optional로 감싸줌
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return store.values().stream()
+                .filter(member -> member.getName().equals(name))
+                .findAny();
+        // 결과는 Optional로 반환.
+        // roof를 다 돌면서 찾으면 반환
+        // 없으면 Optional에 Null이 포함되어 반환
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+
+    public void clearStore(){
+        store.clear();
+    }
+}
